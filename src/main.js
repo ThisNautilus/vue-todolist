3699d2679a75cd65bc391ae2d8210ae493081b0b
ThisNautilus <zziaocean@foxmail.com>
2019-08-27 21:39:01 +0800

    save2138

--- src/main.js
+++ src/main.js
@@ -3,5 +3,14 @@ import 'todomvc-app-css/index.css'
 import Vue from 'vue'
 
 new Vue({
-    el: 'todoapp'
+    el: '.todoapp',
+    data: {
+        msg: '',
+        title: '待办清单',
+        newTodo: ''
+    }
 })
+
+// new Vue({
+//     el: '.info'
+// })
